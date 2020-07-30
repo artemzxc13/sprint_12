af1eb56029a5c043df9839573f7094dd45155838
artemzxc13 <artyom.kharitonov@27>
2020-07-31 01:45:34 +0300

    working updates

---
+++ middle/users.js
@@ -0,0 +1,25 @@
+const fs = require('fs').promises;
+
+const users = ({ ...object }) => {
+  fs.readFile(object.dataPath, { encoding: 'utf8' })
+    .then((data) => {
+      if (!object.req) {
+        return object.res.send(JSON.parse(data));
+      }
+      const parsenData = JSON.parse(data);
+
+      const userId = (id) => parsenData.find((user) => id === user._id);
+      const user = userId(object.req.params.id);
+      if (user) {
+        return object.res.send(user);
+      }
+      return object.res.status(404).send({
+        message: ' Нет такого пользователя с таким id',
+      });
+    })
+    .catch(() =>
+      object.res.status(500).send({ message: 'Ошибка чтения файла' })
+    );
+};
+
+module.exports = users;
